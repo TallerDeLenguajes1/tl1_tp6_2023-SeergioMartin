6979959e48ef66b7cfde863223ba32270202b550
Sergio03 <mariano13martin@gmail.com>
2023-05-29 20:54:10 -0300

    Punto 2 completado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -36,3 +36,76 @@ if (num > 0)
 */
 
 // EJERCICIO 2
+string? cadena;
+int opcion = 0, otra = 0;
+float num1, num2, resultado = 0;
+bool valido = false;
+do
+{
+    if (otra != 1)
+    {
+        System.Console.WriteLine("*****MENU*****");
+        System.Console.WriteLine("1. Sumar");
+        System.Console.WriteLine("2. Restar");
+        System.Console.WriteLine("3. Multiplicar");
+        System.Console.WriteLine("4. Dividir");
+        System.Console.WriteLine("5. Salir");
+        System.Console.WriteLine("--Seleccione una opcion:");
+        cadena = Console.ReadLine();
+        int.TryParse(cadena, out opcion);
+    }
+    switch (opcion)
+    {
+        case 1:
+            num1 = Numeros();
+            num2 = Numeros();
+            resultado = num1 + num2;
+            break;
+        case 2:
+            num1 = Numeros();
+            num2 = Numeros();
+            resultado = num1 - num2;
+            break;
+        case 3:
+            num1 = Numeros();
+            num2 = Numeros();
+            resultado = num1 * num2;
+            break;
+        case 4: 
+            num1 = Numeros();
+            num2 = Numeros();
+            if (num2 != 0)
+            {
+                resultado = num1 / num2;
+            }else
+            {
+                System.Console.WriteLine("No se puede dividir un numero en 0!");
+            }
+            break;
+        case 5:
+            break;
+    }
+    if (opcion >=1 && opcion <=4){
+        System.Console.WriteLine("El Resultado es: " + resultado);
+        Console.WriteLine("¿Desea realizar otro calculo?\n1. Si\n2. No");
+        System.Console.WriteLine("--Seleccione una opcion:");
+        cadena = Console.ReadLine();
+        int.TryParse(cadena, out otra);
+    }
+} while (opcion != 5);
+
+float Numeros(){
+    float num = 0;
+    while (!valido)
+    {
+        Console.WriteLine("Ingrese un numero:");
+        cadena = Console.ReadLine();
+        valido = float.TryParse(cadena, out num);
+        if (!valido)
+        {
+            Console.WriteLine("Numero ingresado no valido!");
+        }
+    }
+    valido = false;
+    return num;
+}
